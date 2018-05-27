212edc013aabfaabe170a18af273571f77267879
barkam75 <kaminskb@gmail.com>
2018-05-27 21:09:29 +0000

    WT Forms working

---
+++ author/form.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+class RegisterForm(FlaskForm):
+    fullname = StringField('Full Name',[validators.Required()])
+    email = EmailField('Email', [validators.Required()])
+    username = StringField('Username', [
+        validators.Required(),
+        validators.Length(min=4,max=25)
+        ])
+    password = PasswordField('New Password',[
+        validators.Required(),
+        validators.EqualTo('confirm', message="Passwords do not match"),
+        ])
+    confirm = PasswordField('Repeat Password')
+    
---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY='yo will never guess'
+DEBUG = True
+DB_USERNAME = 'db_root'
+DB_PASSWORD = 'db_pwd'
+BLOG_DATABASE_NAME = 'flask_blog'
+DB_HOST = os.getenv('IP','0.0.0.0')
+DB_URI="mysql+pymysql://%s:%s@%s/%s" %(DB_USERNAME, DB_PASSWORD,DB_HOST,BLOG_DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
