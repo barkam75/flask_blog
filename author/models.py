aa82f8af3daa686e5ad024b0febed9d298fbf319
Bartek Kaminski <kaminskb@gmail.com>
2018-06-13 22:10:23 +0200

    Article view added

--- author/models.py
+++ author/models.py
@@ -7,13 +7,17 @@ class Author(db.Model):
     username=db.Column(db.String(80), unique=True)
     password=db.Column(db.String(60))
     is_author = db.Column(db.Boolean)
-     
+    
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __init__(self, fullname, email, username, password, is_author=False):
         self.fullname = fullname
         self.email=email
         self.username=username
         self.password=password
         self.is_author=is_author
+
+        
     
     def __repr__(self):
         return '<Author %r>' %self.username
--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,8 @@ class Post(db.Model):
     publish_date = db.Column(db.DateTime)
     live=db.Column(db.Boolean)
     category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
-
+    category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
+    
     def __init__(self, blog,author, title, body,category, slug=None, publish_date=None, live=True):
         self.blog_id = blog.id
         self.author_id=author.id
@@ -41,6 +42,7 @@ class Post(db.Model):
 class Category(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(50))
+
 
      def __init__(self, name):
          self.name = name
